f296e550937d32984c230dc79516e77fd574fc8d
Erik Hansen <e.hansen31@live.com>
2020-02-22 12:26:46 -0500

    save component children

--- components/src/index.js
+++ components/src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from "./ApprovalCard"
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 4:45"
-                imageURL={faker.image.avatar()}
-                text="first text here"
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:45"
+                    imageURL={faker.image.avatar()}
+                    text="first text here"
+                />
+            </ApprovalCard>
+
             <CommentDetail
                 author="Jane"
                 timeAgo="Today at 2:00AM"
