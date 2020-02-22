d323d0e0ba0808b078357752d7eeba1d84a0a693
erik <ehansen1231@gmail.com>
2020-02-21 22:09:59 -0500

    save changes

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,16 @@ import ReactDOM from 'react-dom';
 
 // create a react component
 const App = () => {
-    return <div>Hi There!</div>
+    let buttonText = 'click me!';
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
+        </div>
+    );
 };
 
 // take the react component and show it on the screen
