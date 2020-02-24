385416a440ffa078db6d82bcff3862023d01ccc4
Unknown <e.hansen31@live.com>
2020-02-24 00:18:28 -0500

    class based components, state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => { console.log(position) },
-        (err) => { console.log(err); }
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        };
+    }
 
-    return (
-        <div>
-            Hi there!
-        </div>
-    );
-};
+    // react says we have to define render!!
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => { this.setState({ lat: position.coords.latitude }); },
+            (err) => { this.setState({ errorMessage: err.message }); }
+        );
+
+        return (
+            <div>
+                Latitude: {this.state.lat}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
     <App />,
