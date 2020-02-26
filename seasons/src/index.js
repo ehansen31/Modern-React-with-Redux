2ed242912c49ac1487e2c3c945211e9b37e3e382
Erik Hansen <e.hansen31@live.com>
2020-02-25 22:07:44 -0500

    ternary ui update

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            lat: null,
-            errorMessage: ''
-        };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         lat: null,
+    //         errorMessage: ''
+    //     };
+    // }
+    state = { lat: null, errorMessage: '' }
 
-    // react says we have to define render!!
-    render() {
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => { this.setState({ lat: position.coords.latitude }); },
             (err) => { this.setState({ errorMessage: err.message }); }
         );
+    }
 
-        return (
-            <div>
-                Latitude: {this.state.lat}
-                <br />
-                Error: {this.state.errorMessage}
-            </div>
-        );
+    componentDidUpdate() {
+        console.log('my component was just updated');
+    }
+
+    // react says we have to define render!!
+    render() {
+
+
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <div>Loading...</div>;
     }
 }
 
