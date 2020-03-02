8b76a72da32d9873c211efd63784f39104626677
Erik Hansen <e.hansen31@live.com>
2020-03-01 23:13:05 -0500

    save changes, passing children functions

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,22 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
 
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+    onFormSubmit(event) {
+        event.preventDefault();
 
-    onInputClick() {
-        console.log('Input was clicked')
+        this.props.onSubmit(this.state.term)
     }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={(event) => { this.onFormSubmit(event) }} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} onClick={this.onInputClick} />
+                        <input
+                            type="text"
+                            onChange={(e) => { this.setState({ term: e.target.value }) }}
+                            value={this.state.term}
+                        />
                     </div>
                 </form>
             </div>
